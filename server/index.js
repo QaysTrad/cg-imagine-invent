5178a46485bafa78bb428f67861892ade8d3e555
QaysTrad <qaystrad@gamil.com>
2018-06-22 15:04:17 +0400

    done setup all files

---
+++ server/index.js
@@ -0,0 +1,17 @@
+let express = require('express') 
+let path = require('path') 
+
+const app = express() 
+
+app.use(express.static(path.join(__dirname, '../react-client/dist'))) 
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(path.join(__dirname, '../react-client/dist/index.html')))
+}) 
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`this app is running on port ${ PORT }`);
+});
+
+module.exports = app
