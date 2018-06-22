7ac6a145cbfd97a92f53c36e8fd6105284d0c735
QaysTrad <qaystrad@gamil.com>
2018-06-22 15:18:42 +0400

    add  component and route to react

---
+++ react-client/src/components/AppRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Route, hashHistory, Switch } from 'react-router-dom'
+
+import Home from './Home.jsx'
+
+class AppRoute extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = { }
+	}
+	render (){
+		return (
+			      <BrowserRouter history={hashHistory}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        
+        </Switch>
+        </BrowserRouter>
+			)
+	}
+}
+
+export default AppRoute
---
+++ react-client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+class Home extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+	}
+render(){
+	return (
+		<div>
+		<h1>This is me agian</h1>
+		</div>
+
+		)
+}
+}
+
+export default Home
