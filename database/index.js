dd941873aa94b148d7d4327ae0146d8269d90d8d
QaysTrad <qaystrad@gamil.com>
2018-06-24 01:44:13 +0400

    fix code style

--- database/index.js
+++ database/index.js
@@ -1,29 +1,30 @@
-var mongoose = require('mongoose') 
- mongoose.connect('mongodb://localhost/assgin') 
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/assgin')
 
-var db = mongoose.connection 
+var db = mongoose.connection
 
 db.on('error', function () {
   console.log('mongoose connection error')
-}) 
-db.once('open', function () {
-  console.log('mongoose connected successfully')
-}) 
-
-var user = mongoose.Schema({
-  username: { type : String, required : true , unique : true},
-  email:{ type : String, required : true },
-  password: { type : String, required : true },
-});
-
-var task = mongoose.Schema({
-	taskName : {type : String , required : true},
-	complete : {type : Boolean },
-	username : {type : String }
 })
 
-var User = mongoose.model('User', user);
-var Task = mongoose.model('Task', task);
+db.once('open', function () {
+  console.log('mongoose connected successfully')
+})
+
+var user = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true },
+  password: {type: String, required: true }
+})
+
+var task = mongoose.Schema({
+  taskName: {type: String, required: true},
+  complete: {type: Boolean },
+  username: {type: String }
+})
+
+var User = mongoose.model('User', user)
+var Task = mongoose.model('Task', task)
 
 module.exports.User = User
 module.exports.Task = Task
